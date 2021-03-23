c5ce14953d55e146cb85258780143b18c8618760
Antiquiti <pawel659@onet.eu>
2021-03-23 20:08:22 +0100

    Initialization

---
+++ todo-list/resources/views/welcome.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        
+    </head>
+
+    <body>
+        <div id="app">
+            
+
+            <example-component></example-component>
+
+        
+        </div>
+        <script src="js/app.js"></script>
+    </body>
+</html>
